80833d8475cd42df8f2bf6db88b6ca9844c8b475
Bheki <bheki.daweti@gmail.com>
2025-08-08 19:24:11 +0200

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Bheki Daweti - Full Stack Developer',
@@ -11,6 +12,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         <header className="p-4">
+          {/* Google Analytics (GA4) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K1BSS4M7ZV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-K1BSS4M7ZV');
+          `}
+        </Script>
   
         </header>
         {children}

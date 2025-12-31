864b7ea007ac5893aaaef116ca82fdf8107683d1
bhekidaweti <99840968+bhekidaweti@users.noreply.github.com>
2025-12-31 02:24:26 +0200

    Integrate Iubenda for cookie policy management
    Added Iubenda script for cookie compliance.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', 'G-K1BSS4M7ZV');
           `}
         </Script>
+        <script type="text/javascript">
+          var _iub = _iub || [];
+          _iub.csConfiguration = {"siteId":4369685,"cookiePolicyId":76409301,"lang":"en","storage":{"useSiteId":true}};
+          </script>
+          <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/4369685.js"></script>
+          <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+          <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async></script>
       </header>
       <body >
       <Header />

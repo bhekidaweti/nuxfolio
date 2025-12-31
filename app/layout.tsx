60cbb5cfc0c9ecfed4834c014bbcd327e002e682
bhekidaweti <99840968+bhekidaweti@users.noreply.github.com>
2025-12-31 02:14:45 +0200

    Integrate Google Analytics into RootLayout
    Added Google Analytics script to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import {Providers} from './providers'
 import Header from '../components/Header';
+import Script from "next/script";
 
 export const metadata = {
   title: 'Bheki Daweti - Full Stack Developer',
@@ -11,6 +12,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <header>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-K1BSS4M7ZV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-K1BSS4M7ZV');
+          `}
+        </Script>
+      </header>
       <body >
       <Header />
       <Providers>
